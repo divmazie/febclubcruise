79e326af664943540bdc6014337b6d559f3fae40
Drew Westphal <drewwestphal@users.noreply.github.com>
2015-06-05 17:14:34 -0400

    add cruise control support (RUDIMENTARY & POORLY TESTED)

---
+++ canvas.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head
+        content must come *after* these tags -->
+        <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png" />
+        <?php
+        printf('<title>%s</title>', wp_title(' | ', false, 'right'));
+        ?>
+
+        <?php wp_head(); ?>
+    </head>
+    <body>
+        <section class="canvas" id="page-canvas">
+            <div class="container">
+                <?php
+                @the_content();
+                //include 'analytics.php';
+                wp_footer();
+                ?>
+            </div>
+        </section>
+        <footer>
+            <div class="footer-info">
+            </div>
+        </footer>
+
+    </body>
+</html>
