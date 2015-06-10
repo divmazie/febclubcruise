1f9978710e7fc532d5a5cdbbecb716598c9444dd
David Mackenzie <davidamackenzie@gmail.com>
2015-06-10 14:01:44 -0400

    Nav under banner & single post page

--- top.php
+++ top.php
@@ -1,12 +1,13 @@
-	<nav id="menu" role="navigation">
-		<?php wp_nav_menu( array( 'theme_location' => 'main-menu', 
-							  'depth' => 2,
-							  'container' => false,
-							  'menu_class' => 'nav nav-pills',
-							  //Process nav menu using our custom nav walker
-							  'walker' => new wp_bootstrap_navwalker() ) ); 
-		?>
-	</nav>
-	<br/>
-	<img src="<?php echo get_template_directory_uri(); ?>/img/banner-crop.jpg" class="img-responsive" alt="Hover text provided courtesy Randall Munroe">
-	<br/>
+<br/>
+<img src="<?php echo get_template_directory_uri(); ?>/img/banner-crop.jpg" class="img-responsive" alt="Hover text provided courtesy Randall Munroe">
+<br/>
+<nav id="menu" role="navigation">
+	<?php wp_nav_menu( array( 'theme_location' => 'main-menu', 
+						  'depth' => 2,
+						  'container' => false,
+						  'menu_class' => 'nav nav-pills',
+						  'fallback_cb' => 'wp_bootstrap_navwalker::fallback',
+						  //Process nav menu using our custom nav walker
+						  'walker' => new wp_bootstrap_navwalker() ) ); 
+	?>
+</nav>
