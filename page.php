85403fc3a42b6711e481cf40375b681d9ca66cb6
David Mackenzie <davidamackenzie@gmail.com>
2015-06-10 17:29:49 -0400

    Show all FAQs on FAQ page

--- page.php
+++ page.php
@@ -16,7 +16,29 @@
 				<div class="entry-links"><?php wp_link_pages(); ?></div>
 			</section>
 		</article>
-		<?php endwhile; endif; ?>
+		<?php endwhile; endif; 
+		// Output all the FAQs in the FAQ section
+		if ($post->post_name == "faq") {
+			$faq_posts = new WP_Query( array(
+			    'post_type' => 'faq',
+			    'posts_per_page' => -1,
+			));
+			$faq_posts = $faq_posts -> get_posts();
+			wp_reset_postdata();
+			echo "<div class='indent'>";
+			foreach ($faq_posts as $faq_post) {
+				$name = $faq_post->post_name;
+				$title = $faq_post->post_title;
+				if (strpos(" ".$title,"HIDE TITLE")) {
+					$title = "";
+				}
+				$content = apply_filters('the_content',$faq_post->post_content);
+				$div_tags = " id=$name";
+				echo $twig->render('faq_item.html', array('title' => $title, 'content' => $content));
+			}
+			echo "</div>";
+		}
+		?>
 </section>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
