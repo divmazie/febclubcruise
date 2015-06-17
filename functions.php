213e8caef97d8f0a37a246bbbe1b01eeef052659
David Mackenzie <davidamackenzie@gmail.com>
2015-06-17 15:49:54 -0400

    Google Analytics, set ID in theme options #13

--- functions.php
+++ functions.php
@@ -95,15 +95,24 @@ $twig = new Twig_Environment($loader, array(
 require_once(__DIR__.'/gallery.php');
 
 add_action('wp_footer', function(){
-        echo <<<EOF
-        <script type="text/javascript">
-            // open external links in new tab
-            $('#content').find('a').filter(function() {
-                return this.hostname && this.hostname.indexOf(location.hostname)===-1
-            }).attr({
-                target : "_blank"
-            });
-        </script>
+	$google_analytics_code = cctheme_get_option('ga_id');
+    echo <<<EOF
+    <script type="text/javascript">
+        // open external links in new tab
+        $('#content').find('a').filter(function() {
+            return this.hostname && this.hostname.indexOf(location.hostname)===-1
+        }).attr({
+            target : "_blank"
+        });
+		
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		
+		  ga('create', '$google_analytics_code', 'auto');
+		  ga('send', 'pageview');
+    </script>
 EOF;
 });
 ?>
