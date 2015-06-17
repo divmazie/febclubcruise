61492ae46174def1340ab2d348a0ad453d83b13e
David Mackenzie <davidamackenzie@gmail.com>
2015-06-17 15:29:35 -0400

    Delete commented code in index.php

--- index.php
+++ index.php
@@ -11,29 +11,7 @@
 	require 'top.php';
 	$front_page = get_page_by_path('front-page', OBJECT, 'themepage');
 	echo $twig->render('article.html', array('title'=>$front_page->post_title, 'content'=>apply_filters('the_content',$front_page->post_content), 'div_tags' => " id=".$front_page->ID));
-	
-	/*
-	$front_posts = new WP_Query( array(
-	    'post_type' => 'front_page_item',
-	    'posts_per_page' => -1,
-	));
-	$front_posts = $front_posts -> get_posts();
-	wp_reset_postdata();
-	
-	foreach($front_posts as $post) {
-		//var_dump($post);
-		//echo "<br />".$post->post_content."<br />";
-		$name = $post->post_name;
-		$title = $post->post_title;
-		if (strpos(" ".$title,"HIDE TITLE")) {
-			$title = "";
-		}
-		$content = apply_filters('the_content',$post->post_content);
-		$div_tags = " id=$name";
-		echo $twig->render('article.html', array('title' => $title, 'content' => $content, 'div_tags' => $div_tags));
-	}
-	 * 
-	 */
+
 	// Output all the FAQs
 	$faq_posts = new WP_Query( array(
 	    'post_type' => 'faq',
