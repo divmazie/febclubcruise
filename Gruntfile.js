931b0a1f0af345e8ba8daa4947713f1071ffaf43
Drew Westphal <drewwestphal@users.noreply.github.com>
2015-06-17 16:41:38 -0400

    autoformat

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,47 +1,43 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-	   bower: {
-	    options: {
-	      mangle: true,
-	      compress: true
-	    },
-	    files: {
-	      'js/bower.min.js': 'js/bower.js'
-	    }
-	  },
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    },
-	bower_concat: {
-	  all: {
-	    dest: 'js/bower.js'
-	    //cssDest: 'build/_bower.css'
-	  }
-	}
-  });
-  
-  require('load-grunt-tasks')(grunt);
+    // Project configuration.
+    grunt.initConfig({
+        pkg : grunt.file.readJSON('package.json'),
+        uglify : {
+            bower : {
+                options : {
+                    mangle : true,
+                    compress : true
+                },
+                files : {
+                    'js/bower.min.js' : 'js/bower.js'
+                }
+            },
+            options : {
+                banner : '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build : {
+                src : 'src/<%= pkg.name %>.js',
+                dest : 'build/<%= pkg.name %>.min.js'
+            }
+        },
+        bower_concat : {
+            all : {
+                dest : 'js/bower.js'
+                //cssDest: 'build/_bower.css'
+            }
+        }
+    });
 
+    require('load-grunt-tasks')(grunt);
 
-  // Load the plugin that provides the "uglify" task.
-  //grunt.loadNpmTasks('grunt-contrib-uglify');
-  //grunt.loadNpmTasks('grunt-bower-concat');
+    // Load the plugin that provides the "uglify" task.
+    //grunt.loadNpmTasks('grunt-contrib-uglify');
+    //grunt.loadNpmTasks('grunt-bower-concat');
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-  
-   grunt.registerTask('buildbower', [
-	  'bower_concat',
-	  'uglify:bower'
-	]);
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
 
-};
+    grunt.registerTask('buildbower', ['bower_concat', 'uglify:bower']);
+
+}; 
